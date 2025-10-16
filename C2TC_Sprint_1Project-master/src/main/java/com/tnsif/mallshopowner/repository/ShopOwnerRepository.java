2f38fd3883fefeb17dbbb4ac8634617e26672435
devanathan471 <devanathans828@gmail.com>
2025-10-16 23:06:11 +0530

    Delete C2TC_Sprint_1Project-master directory

--- C2TC_Sprint_1Project-master/src/main/java/com/tnsif/mallshopowner/repository/ShopOwnerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tnsif.mallshopowner.repository;
-
-import com.tnsif.mallshopowner.entity.ShopOwnerApplication;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopOwnerRepository extends JpaRepository<ShopOwnerApplication, Long> {
-}
